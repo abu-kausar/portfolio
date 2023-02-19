20e0f93e58fbd9c485a45c2710ac3e2015b5a10f
abu-kausar <abukausar33@gmail.com>
2023-02-19 18:01:25 +0600

    service section started

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,10 +1,158 @@
 import React from 'react';
+import './Services.css';
 
 const Services = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="services section" id='services'>
+            <h1 className="section__title">Services</h1>
+            <span className='section__subtitle'>What I offer</span>
+
+            <div className="services__container container grid">
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Lorem, ipsum dolor.</h3>
+                    </div>
+
+                    <span className="services__button">View More
+                        <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+                            
+                            <h3 className="services__modal-title">Lorem, ipsum.</h3>
+                            <p className="services__modal-description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo mollitia recusandae dolore accusamus aut cum asperiores, illo magni totam debitis ipsam saepe! Iusto quis inventore possimus ducimus porro pariatur quos.
+                            </p>
+
+                            <ul className="services__modal-servicesgrid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-arrow services__icon"></i>
+                        <h3 className="services__title">Lorem, ipsum dolor.</h3>
+                    </div>
+
+                    <span className="services__button">View More
+                        <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+                            
+                            <h3 className="services__modal-title">Lorem, ipsum.</h3>
+                            <p className="services__modal-description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo mollitia recusandae dolore accusamus aut cum asperiores, illo magni totam debitis ipsam saepe! Iusto quis inventore possimus ducimus porro pariatur quos.
+                            </p>
+
+                            <ul className="services__modal-servicesgrid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-edit services__icon"></i>
+                        <h3 className="services__title">Lorem, ipsum dolor.</h3>
+                    </div>
+
+                    <span className="services__button">View More
+                        <i className="uil uil-arrow-right services__button-icon"></i>
+                    </span>
+
+                    <div className="services__modal">
+                        <div className="services__modal-content">
+                            <i className="uil uil-times services__modal-close"></i>
+                            
+                            <h3 className="services__modal-title">Lorem, ipsum.</h3>
+                            <p className="services__modal-description">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo mollitia recusandae dolore accusamus aut cum asperiores, illo magni totam debitis ipsam saepe! Iusto quis inventore possimus ducimus porro pariatur quos.
+                            </p>
+
+                            <ul className="services__modal-servicesgrid">
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+
+                                    <p className="services__modal-info">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,7 +6,7 @@ import './Skills.css';
 const Skills = () => {
     return (
         <section className="skills section" id='skills'>
-             <h1 className="section__title">Skills</h1>
+            <h1 className="section__title">Skills</h1>
             <span className='section__subtitle'>My technical level</span>
 
             <div className="skills__container container grid">
