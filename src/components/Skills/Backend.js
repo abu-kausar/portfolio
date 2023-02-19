da199d5f9aa454687c0d3b9d9009f837f96f253f
abu-kausar <abukausar33@gmail.com>
2023-02-19 16:45:57 +0600

    skills component added and done

---
+++ src/components/Skills/Backend.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Backend = () => {
+    return (
+        <div className="skills__content">
+            <h3 className="skills__title">Backend Developer</h3>
+
+            <div className="skills__box">
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">Node JS</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">Python</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">PHP</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="skills__group">
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">Express JS</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">Firebase</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i className='bx bx-badge-check'></i>
+
+                        <div>
+                            <div className="skills_name">MongoDB</div>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Backend;
