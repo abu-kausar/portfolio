4bc6182c47d792aabc0d098715b2e9a7f920766d
abu-kausar <abukausar33@gmail.com>
2023-02-24 23:40:02 +0600

    contact me section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Qualification from './components/Qualification/Qualification';
@@ -18,6 +19,7 @@ function App() {
       <Services/>
       <Qualification/>
       <Testimonial/>
+      <Contact/>
     </main>
     </>
   );
