5e1146cfe352daf8fab51055dda47542e520037e
abu-kausar <abukausar33@gmail.com>
2023-03-08 03:21:45 +0600

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
 
                 <ul className="footer__list">
                     <li><a href="#about" className="footer__link">About</a></li>
-                    <li><a href="#projects" className="footer__link">Portfolio</a></li>
-                    <li><a href="#testimonials" className="footer__link">Testimonials</a></li>
+                    <li><a href="#projects" className="footer__link">Projects</a></li>
+                    <li><a href="#qualification" className="footer__link">Qualification</a></li>
                 </ul>
 
                 <div className="footer__social">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -71,9 +71,9 @@ const Header = () => {
                         </li>
 
                         <li className="nav__item">
-                            <a href="#portfolio"
-                                onClick={() => setActiveNav('#portfolio')}
-                                className={activeNav === '#portfolio' ?
+                            <a href="#qualification"
+                                onClick={() => setActiveNav('#qualification')}
+                                className={activeNav === '#qualification' ?
                                     'nav__link active-link' :
                                     'nav__link'}>
                                 <i className="uil uil-scenery nav__icon"></i>Qualification
